22602ef5aa2145d5a20dbffd4832c191f28d0f25
Breno Morais <brenomorais88@gmail.com>
2025-07-08 18:29:08 -0300

    Create initial project

---
+++ PandaScoreApp/Features/Matches/Presentation/Views/MatchListView.swift
@@ -0,0 +1,25 @@
+//
+//  MatchListView.swift
+//  PandaScoreApp
+//
+//  Created by Breno Morais on 08/07/25.
+//
+
+import SwiftUI
+
+struct MatchListView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MatchListView()
+}
+
---
+++ PandaScoreApp/PandaScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  PandaScoreAppApp.swift
+//  PandaScoreApp
+//
+//  Created by Breno Morais on 08/07/25.
+//
+
+import SwiftUI
+
+@main
+struct PandaScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MatchListView()
+        }
+    }
+}
