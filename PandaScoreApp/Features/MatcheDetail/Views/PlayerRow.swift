33d10a1e9915c07ff1431ad2f9d2f52f012bce26
Breno Morais <brenomorais88@gmail.com>
2025-07-23 13:45:57 -0300

    remove duplicated PlayerRow

--- PandaScoreApp/Features/MatcheDetail/Views/PlayerRow.swift
+++ PandaScoreApp/Features/MatcheDetail/Views/PlayerRow.swift
@@ -13,12 +13,12 @@ struct PlayerRow: View {
 
     var body: some View {
         HStack() {
-            PlayerCellLeft(player: left)
+            PlayerCell(position: .left, player: left)
 
             Spacer()
                 .frame(width: 24)
 
-            PlayerCellRight(player: right)
+            PlayerCell(position: .right, player: right)
         }
         .background(Color.clear)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -26,74 +26,68 @@ struct PlayerRow: View {
     }
 }
 
-private struct PlayerCellLeft: View {
+enum PlayerCellPosition {
+    case right
+    case left
+}
+
+private struct PlayerCell: View {
+    let position: PlayerCellPosition
     let player: MatchDetail.Player?
 
     var body: some View {
+        if position == .left {
+            playerLeftView
+        } else {
+            playerRightView
+        }
+    }
+
+    private var playerLeftView: some View {
         HStack {
             Spacer()
-
-            VStack(alignment: .trailing, spacing: 4) {
-                Text(player?.name ?? "-")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .lineLimit(1)
-                Text(player?.firstName ?? "-")
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
-                    .lineLimit(1)
-            }
-
-            AsyncImage(url: player?.imageUrl) { phase in
-                if let image = phase.image {
-                    image
-                        .resizable()
-                        .scaledToFill()
-                } else {
-                    PlaceholderPlayerView()
-                }
-            }
-            .frame(width: 52, height: 52)
-            .padding(8)
-            .clipShape(RoundedRectangle(cornerRadius: 6))
+            textView
+            playerImage
         }
         .background(AppColors.card)
         .clipShape(RoundedCorner(radius: 8, corners: [.topRight, .bottomRight]))
     }
-}
 
-private struct PlayerCellRight: View {
-    let player: MatchDetail.Player?
-
-    var body: some View {
+    private var playerRightView: some View {
         HStack {
-            AsyncImage(url: player?.imageUrl) { phase in
-                if let image = phase.image {
-                    image
-                        .resizable()
-                        .scaledToFill()
-                } else {
-                    PlaceholderPlayerView()
-                }
-            }
-            .frame(width: 52, height: 52)
-            .padding(8)
-            .clipShape(RoundedRectangle(cornerRadius: 6))
-
-            VStack(alignment: .leading, spacing: 4) {
-                Text(player?.name ?? "-")
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .lineLimit(1)
-                Text(player?.firstName ?? "-")
-                    .font(.subheadline)
-                    .foregroundColor(.gray)
-                    .lineLimit(1)
-            }
-
+            playerImage
+            textView
             Spacer()
         }
         .background(AppColors.card)
         .clipShape(RoundedCorner(radius: 8, corners: [.topLeft, .bottomLeft]))
+    }
+
+    private var textView: some View {
+        VStack(alignment: .trailing, spacing: 4) {
+            Text(player?.name ?? "-")
+                .font(.headline)
+                .foregroundColor(.white)
+                .lineLimit(1)
+            Text(player?.firstName ?? "-")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+                .lineLimit(1)
+        }
+    }
+
+    private var playerImage: some View {
+        AsyncImage(url: player?.imageUrl) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFill()
+            } else {
+                PlaceholderPlayerView()
+            }
+        }
+        .frame(width: 52, height: 52)
+        .padding(8)
+        .clipShape(RoundedRectangle(cornerRadius: 6))
     }
 }
